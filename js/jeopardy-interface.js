fb1e8f52a01aa18828d56e1d66cba1c9409ec263
Stephanie Mayer <smayer0926@gmail.com>
2017-10-02 11:07:44 -0700

    add question object and test

---
+++ js/jeopardy-interface.js
@@ -0,0 +1,5 @@
+// import Question = require('./../js/jeopardy.js').questionModule;
+import {Question} from './../js/jeopardy.js';
+
+let q1 = new Question("2+2", "4", "300", 1, 1, "Arithmetic");
+let q2 = new Question("Fastest land mammal", "Cheetah", "500", 2, 2, "Mammals");
---
+++ js/jeopardy.js
@@ -0,0 +1,10 @@
+export class Question {
+  constructor (question, answer, value, id, categoryId, categoryTitle) {
+    this.question = question;
+    this.answer = answer;
+    this.value = value;
+    this.id = id;
+    this.categoryId = categoryId;
+    this.categoryTitle = categoryTitle;
+  }
+}
